e41daf0d5be05e9890534ebadf093318f2721a15
kazhida <kazhida@abplus.com>
2019-07-06 15:57:23 +0900

    いろいろ調整

--- keyboards/ergodash/rev1/keymaps/kazhida/keymap.c
+++ keyboards/ergodash/rev1/keymaps/kazhida/keymap.c
@@ -21,76 +21,47 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /* Qwerty
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | Esc  | Esc  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |   -  |   BS |
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | Home | Tab  |  Q   |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |   @  |   [  |
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | PgUp | Ctrl | 	A   |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   ;  |   :  |   ]  |
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | PgDn | Shift| 	Z   |   X  |   C  |   V  |   B  |                    |   N  |   M  |   ,  |   .  |   /  |   ▲  | Shift|
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | End  | Opt  | Cmd  | |||| | Raise| Eisu | Cmd  | Space| |||| |  Ent | Kana | Opt  | Lower| |||| |  ◀  |   ▼  |  ▶  |
-   * ,----------------------------------------------------------------------------------------------------------------------.
+  /*
+   * Qwerty
    */
   [_QWERTY] = LAYOUT( \
-    KC_ESC,  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS,JP_YEN, \
-    KC_HOME, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,   JP_LBRC, \
-    KC_PGUP, KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, JP_RBRC, \
-    KC_PGDN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_RSFT, \
-    KC_END,  KC_LALT, KC_LGUI,          KC_SPC,  KC_SPC,  RAISE,   KC_SPC,           KC_ENT,  LOWER,   KC_ENT,           KC_RO,   KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_RO,                           JP_MINS,   KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_BSPC,  \
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     JP_CIRC,                         JP_YEN,    KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    JP_AT,    \
+    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     JP_LBRC,                         JP_RBRC,   KC_H,     KC_J,     KC_K,    KC_L,    JP_SCLN, JP_COLN,  \
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     M_EISU,                          M_KANA,    KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_UP,   KC_ENT, \
+    KC_LALT,  KC_LGUI,  KC_LGUI,            KC_TAB,   RAISE,    KC_LSFT,  KC_SPC,       KC_ENT,  KC_RSFT,   LOWER,              KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
-  /* Lower
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | Esc  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   BS |
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | Home | Tab  |  Q   |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |   @  |   [  |
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | PgUp | Ctrl | 	A   |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   ;  |   :  |   ]  |
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | PgDn | Shift| 	Z   |   X  |   C  |   V  |   B  |                    |   N  |   M  |   ^  |   ¥  |   \  | PgUp | Shift|
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | End  | Opt  | Cmd  | |||| | Raise| Eisu | Cmd  | Space| |||| |  Ent | Kana | Opt  | Lower| |||| | Home | PgDn | End  |
-   * ,----------------------------------------------------------------------------------------------------------------------.
+  /*
+   * Lower
    */
   [_LOWER] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                              KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, \
-    _______, _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, KC_ENT,  \
-    _______, _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, KC_ENT,  \
-    _______, _______, _______, _______, _______, _______, _______,                            _______, _______, JP_CIRC, JP_YEN,  _______, KC_PGUP, _______, \
-    KC_END,  KC_LALT, KC_LGUI,          KC_SPC,  KC_SPC,  RAISE,   M_EISU,           M_KANA,  LOWER,   KC_ENT,           KC_RO,   KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_BSPC, \
+    KC_TAB,   KC_EXLM,  JP_DQT,   KC_HASH,  KC_DLR,   KC_PERC,  JP_CIRC,                          JP_YEN,   JP_AMPR,  JP_QUOT,  JP_LPRN,  JP_RPRN,  XXXXXXX,  XXXXXXX, \
+    KC_LCTL,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     JP_LBRC,                          JP_RBRC,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     XXXXXXX, \
+    KC_LSFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  M_EISU,                           M_KANA,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PGUP,  KC_ENT,  \
+    KC_LALT,  KC_LGUI,  KC_LGUI,            KC_TAB,   RAISE,    KC_LSFT,  KC_SPC,       KC_ENT,   KC_RSFT,  LOWER,              KC_SLSH,  KC_HOME,  KC_PGDN,  KC_END   \
   ),
 
-  /* Raise
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | Esc  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   BS |
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | Home | Tab  |  Q   |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |   @  |   [  |
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | PgUp | Ctrl | 	A   |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   ;  |   :  |   ]  |
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | PgDn | Shift| 	Z   |   X  |   C  |   V  |   B  |                    |   N  |   M  |   ~  |   |  |   _  | PgUp | Shift|
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * | End  | Opt  | Cmd  | |||| | Raise| Eisu | Cmd  | Space| |||| |  Ent | Kana | Opt  | Lower| |||| | Home | PgDn | End  |
-   * ,----------------------------------------------------------------------------------------------------------------------.
+  /*
+   * Raise
    */
   [_RAISE] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                              KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, \
-    _______, _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, KC_ENT,  \
-    _______, _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, KC_ENT,  \
-    _______, _______, _______, _______, _______, _______, _______,                            _______, _______, JP_CIRC, JP_YEN,  _______, KC_PGUP, _______, \
-    KC_END,  KC_LALT, KC_LGUI,          KC_SPC,  KC_SPC,  RAISE,   M_EISU,           M_KANA,  LOWER,   KC_ENT,           KC_RO,   KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_BSPC, \
+    KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  JP_TILD,                          JP_PIPE,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  XXXXXXX, \
+    KC_LCTL,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     JP_LCBR,                          JP_RCBR,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     XXXXXXX, \
+    KC_LSFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  M_EISU,                           M_KANA,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PGUP,  KC_ENT,  \
+    KC_LALT,  KC_LGUI,  KC_LGUI,            KC_TAB,   RAISE,    KC_LSFT,  KC_SPC,       KC_ENT,   KC_RSFT,  LOWER,              KC_SLSH,  KC_HOME,  KC_PGDN,  KC_END   \
   ),
 
-  /* Adjust
+  /*
+   * Adjust
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * |      | Reset|RGB ON|  MODE|  HUE-|  HUE+|      |                    |      |  SAT-|  SAT+|  VAL-|  VAL+|      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      |      | BL ON|  BRTG|  INC|   DEC|      |                    |      |      |      |      |      |      |      |
+   * |      |      | BL ON|  BRTG|   INC|   DEC|      |                    |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
